0c5a6aff5f4348b730321ed705eaa7a751120e31
DANNYSTARLORD <161342908+DANNYSTARLORD@users.noreply.github.com>
2026-02-14 22:40:31 -0500

    Ending day 1.

--- Practice/leetcode.cpp
+++ Practice/leetcode.cpp
@@ -7,28 +7,3 @@ using v = vector<T>;
 #define all(x) (x).begin(), (x).end()
 #define sz(x) (int)(x).size()
 
-class Solution
-{
-public:
-    vector<int> toggleLightBulbs(vector<int> &bulbs)
-    {
-        set<int> on;
-        bitset<101> toggle;
-        for (int &bulb : bulbs)
-        {
-            if (toggle[bulb])
-            {
-                on.erase(bulb);
-                toggle[bulb] = 0;
-            }
-            else
-            {
-                on.emplace(bulb);
-                toggle[bulb] = 1;
-            }
-        }
-
-        v<int> ans(all(on));
-        return ans;
-    }
-};
