97e9b9d20e5cd8e99a52d5776256b6a40effa043
Pranav Cheruku <161342908+DANNYSTARLORD@users.noreply.github.com>
2026-02-17 11:11:13 -0500

    End day 1 PTL

--- Practice/cses.cpp
+++ Practice/cses.cpp
@@ -12,31 +12,17 @@ using v = vector<T>;
 #define dbg(x)
 #endif
 
+void bfs(v<int> &board){
+
+}
+
 void solve()
 {
-    int n;
-    cin >> n;
-    v<int> nums(n);
-    for (int &num : nums)
-        cin >> num;
+    int n; cin >> n;
+    v<v<int>> board(n, v<int>(n, INT_MAX));
+    bfs(board);
 
-    // (prefix_sum) / x = divisor
-    // (prefix_sum) / divisor = x
-
-    ll prefix_sum = 0, ans = 0;
-    map<ll, int> divisors;
-    divisors[1] = 1;
-
-    for (int num : nums)
-    {
-        prefix_sum += num;
-
-        if (prefix_sum % n == 0)
-            ans += divisors[prefix_sum / n];
-
-        ++divisors[prefix_sum];
-    }
-    cout << ans;
+    for(int i )
 }
 
 int main()
