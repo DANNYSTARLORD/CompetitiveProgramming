57462e4ad0e9f4bc91d6a3659dbd4d4fd57fba9d
DANNYSTARLORD <161342908+DANNYSTARLORD@users.noreply.github.com>
2026-03-16 19:24:30 -0400

    solved Room Allocations cses

--- Practice/leetcode.cpp
+++ Practice/leetcode.cpp
@@ -6,39 +6,3 @@ using v = vector<T>;
 #define eb emplace_back
 #define all(x) (x).begin(), (x).end()
 #define sz(x) (int)(x).size()
-
-class Solution
-{
-    struct Data
-    {
-        int ones = 0, zeroes = 0;
-    };
-
-public:
-    int longestOnes(vector<int> &nums, int k)
-    {
-        v<Data> prefix_arr(sz(nums) + 1);
-
-        for (int i = 1; i < sz(prefix_arr); ++i)
-        {
-            prefix_arr[i].ones += prefix_arr[i - 1].ones + ((nums[i - 1] == 1) ? 1 : 0);
-            prefix_arr[i].zeroes += prefix_arr[i - 1].zeroes + ((nums[i - 1] == 0) ? 1 : 0);
-        }
-
-        int lo = 0, ans = 0;
-
-        for (int hi = 1; hi < sz(prefix_arr); ++hi)
-        {
-            int ones = prefix_arr[hi].ones - prefix_arr[lo].ones;
-            int zeroes = prefix_arr[hi].zeroes - prefix_arr[lo].zeroes;
-
-            cout << ones << " " << zeroes << '\n';
-
-            if (zeroes > k)
-                ++lo;
-            else
-                ans = max(ones + zeroes, ans);
-        }
-        return ans;
-    }
-};
